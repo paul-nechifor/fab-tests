eba481e076fccaf6237e4c061d5110dc8f7ca8a8
Paul Nechifor <paul@nechifor.net>
2014-12-09 21:04:43 +0000

    Hide mkdir errors.

--- tests/04-remote/fabfile.py
+++ tests/04-remote/fabfile.py
@@ -1,10 +1,10 @@
-from fabric.api import run, cd, env, settings
+from fabric.api import run, cd, env, settings, hide
 
 env.hosts = ['10.10.10.10']
 
 def deploy():
     code_dir = '/tmp/asdf'
-    with settings(warn_only=True):
+    with settings(hide('everything'), warn_only=True):
         run('mkdir {}'.format(code_dir))
     with cd(code_dir):
         run('touch file')
