72817fc8f3fb68f7c6bb5403fe01ed7e279c4bd1
Paul Nechifor <paul@nechifor.net>
2014-12-09 20:29:32 +0000

    Add docstrings.

--- tests/05-multiple/fabfile.py
+++ tests/05-multiple/fabfile.py
@@ -10,21 +10,26 @@ env.key_filename = '~/.vagrant.d/insecure_private_key'
 
 @roles('web')
 def install_app():
+    """Install/update the app source file."""
     put('app', '/tmp')
 
 @roles('web')
 def restart_app():
+    """Restart the app service."""
     run('python /tmp/app/main.py')
 
 @roles('web')
 def deploy_app():
+    """Update and restart the app."""
     install_app()
     restart_app()
 
 @roles('db')
 def migrate_db():
+    """Migrate the database."""
     run('echo migrate db')
 
 def full_deploy():
+    """Fully deploy cluster. This would be run on start."""
     execute(migrate_db)
     execute(deploy_app)
